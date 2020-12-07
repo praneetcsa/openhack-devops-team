71c64c39045d9f947b5f0edace6a02fda202fb3c
Praneet Kumar <68839059+praneetcsa@users.noreply.github.com>
2020-12-07 11:45:01 +0530

    Update Program.cs

--- apis/poi/web/Program.cs
+++ apis/poi/web/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using poi.Utility;
-#test-bug
 namespace poi
 {
     public class Program
